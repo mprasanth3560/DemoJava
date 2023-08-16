455515af9878f508c0dfa3c2a7db40eb252f3f88
mprasanth3560 <prasanth.er15@gmail.com>
2023-08-16 10:14:43 +0530

    Java

---
+++ src/org/string/employee.java
@@ -0,0 +1,23 @@
+package org.string;
+
+public class employee {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <5; i++) {
+			for (int j = 0; j < 5; j++) {
+				
+			}
+			System.out.println("i");
+			System.out.println("j");
+		}
+		int a[] = new int[5];
+		a[0] =4;
+		a[1] = 2;
+		a[2] =7;
+		a[4] =6;
+		
+		System.out.println(a[2]);
+		
+	}
+
+}
